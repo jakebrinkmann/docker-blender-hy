b94e2ec86b1adcacd4526aa5b863d9393b00d4c1
Jake Brinkmann <jakebrinkmann@gmail.com>
2019-03-13 17:12:31 -0500

    chore(install): pinned required package versions

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 # Update package-manager mirrors/metadata
 apt-get update --assume-yes
 
+# Install our dependencies
 apt-get install --assume-yes \
-        blender \
-        python3-pip
+        blender=2.79.b \
+        python3-pip=9.0.1-2
 
+# TODO: use pipenv?
 pip install --no-cache \
-    hylang
+    hylang==0.16.0
